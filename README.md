68b5ddc4dd608ec1a5ae051d86386787994ac431
loloexotic <loloexotic@MacBook-Air-de-loloexotic.local>
2023-01-29 22:26:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# contactFormV1
---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+
+  document
+    .querySelector("#contactForm")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      console.log("submit");
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+      };
+
+      /* console.log(data); */
+
+      const response = await axios.post("http://localhost:3000/form", data);
+      console.log(response);
+    });
+});
